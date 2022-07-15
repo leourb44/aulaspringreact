216aac65324bd072bbc9079642aef0f76d85bb2f
Leonardo Urbano <leourb44@gmail.com>
2022-07-14 22:22:33 -0300

    other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from "../../assets/img/logo.svg"
+
+function Header() {
+    return (
+        <>
+            <header>
+                <div className="dsmeta-logo-container">
+                    <img src={logo} alt="DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>
+                        Desenvolvido por
+                        <a href="https://www.instagram.com/urbano_l10/">@urbano_l10</a>
+                    </p>
+                </div>
+            </header>
+        </>
+    )
+}
+export default Header
