a86626c92671960f419b2e2b9611d6191132b39d
Leonardo Urbano <leourb44@gmail.com>
2022-07-15 22:15:39 -0300

    Banco de dados H2

---
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -0,0 +1,76 @@
+package com.devsuperior.dsmeta.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Integer visited;
+	private Integer deals;
+	private String seller_name;
+	private Double amount;
+	private LocalDate date;
+	
+	public Sale()
+	{
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public String getSaler_name() {
+		return saler_name;
+	}
+
+	public void setSaler_name(String saler_name) {
+		this.saler_name = saler_name;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+}
